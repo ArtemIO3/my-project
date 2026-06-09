1612fe0763efb1452210266e6b82a5db28ba8062
artem <artem@gmail.com>
2026-06-09 13:33:54 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Find from './components/Find/Find';
+import Place from './components/Place/Place';
 function App() {
   return (
     <div className="App">
       <Find />
+      <Place/>
     </div>
   );
 }
